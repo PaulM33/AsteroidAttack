95822f123dcb08832de1acf28f9e45aa569b20b3
Daniel Krauskopf <Daniel Krauskopf>
2016-08-13 17:34:56 -0400

    Added main and Asteroid Attack JFrame

---
+++ src/AsteroidAttack.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * Program Name: AsteroidAttack.java
+ * @author Daniel Krauskopf
+ * Purpose:
+ * Date 13-Aug-2016
+ */
+public class AsteroidAttack extends JFrame {
+
+    public AsteroidAttack() {
+        super("Asteroid Attack");
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 800);
+        this.setLocationRelativeTo(null);
+        
+        this.setVisible(true);
+    }
+    
+} // AsteroidAttack;
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * Program Name: Main.java
+ * @author Daniel Krauskopf
+ * Purpose:
+ * Date 13-Aug-2016
+ */
+public class Main {
+    public static void main(String[] args) {
+        new AsteroidAttack();
+    } // main(string[]);
+} // Main;
